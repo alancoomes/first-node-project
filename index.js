ade2069ff0831ddd9eac1ecca56b296c30e5cec8
Alan Coomes <acoomes33@gmail.com>
2023-01-06 19:09:47 -0600

    added middleware functions

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const express = require('express')
 const app  = express()
 
@@ -24,6 +23,16 @@ let notes = [
       important: true
     }
   ]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -76,6 +85,12 @@ app.post('/api/notes', (req, res) => {
     notes = notes.concat(note)
     res.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
 
 
 const PORT = 3001
