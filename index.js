62c5503703158acef0e6e5062888ace3544712f2
Alan Coomes <acoomes33@gmail.com>
2023-01-06 21:28:45 -0600

    starting fly deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app  = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
@@ -93,8 +95,7 @@ const unknownEndpoint = (request, response) => {
   app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
-
     console.log(`Server running on port ${PORT}`)
 })
